fbeb565d2ca0e341464302a0ddebb146e9e53ee6
Aman Ahmed <aman16.aa17@gmail.com>
2023-03-16 11:21:57 +0100

    Controlled Component Implementation

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,29 +1,34 @@
 import { useState } from "react";
 
-function Dropdown({ options }) {
+function Dropdown({ options, selection, onSelect }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
     setIsOpen(!isOpen);
   };
 
-  const handleOptionCLick = (event) => {
+  const handleOptionClick = (option) => {
     setIsOpen(false);
-
-    console.log(event);
+    onSelect(option);
   };
 
   const renderedOptions = options.map((option) => {
     return (
-      <div onClick={() => handleOptionCLick(option)} key={option.value}>
+      <div onClick={() => handleOptionClick(option)} key={option.value}>
         {option.label}
       </div>
     );
   });
 
+  let content = "Select...";
+
+  if (selection) {
+    content = selection.label;
+  }
+
   return (
     <div>
-      <div onClick={handleClick}>Select...</div>
+      <div onClick={handleClick}>{content}</div>
       {isOpen && <div>{renderedOptions}</div>}
     </div>
   );
