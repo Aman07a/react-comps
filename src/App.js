51af6c819d13728a0ad47681eeaab971fca8cf34
Aman Ahmed <aman16.aa17@gmail.com>
2023-03-16 15:53:47 +0100

    A Route Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import Link from "./components/Link";
+import Route from "./components/Route";
+import AccordianPage from "./pages/AccordionPage";
+import DropdownPage from "./pages/DropdownPage";
 
 function App() {
   return (
     <div>
       <Link to="/accordion">Go to accordion</Link>
       <Link to="/dropdown">Go to dropdown</Link>
+      <div>
+        <Route path="/accordion">
+          <AccordianPage />
+        </Route>
+        <Route path="/dropdown">
+          <DropdownPage />
+        </Route>
+      </div>
     </div>
   );
 }
